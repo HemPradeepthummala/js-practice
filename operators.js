30e4bc5d5516e42cc8a991805efc9005a93e0656
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-19 12:45:41 +0530

    operator.js file is created with Arithmetic, logical, and comparision operators

---
+++ operators.js
@@ -0,0 +1,26 @@
+let op1 = 23;
+let op2 = 25;
+
+console.log(op2 , op1);
+
+console.log("Arithmetic operators");
+
+console.log("addition",op1 + op2);
+console.log("subtraction",op1 - op2);
+console.log("multiplication",op1 * op2);
+console.log("division",op1 / op2);
+console.log("",op1 % op2);
+console.log("unary subtraction",-op1);
+
+console.log("Comparision operators");
+console.log("<",op1 < op2);
+console.log(">",op1 > op2);
+console.log("===",op1 === op2);
+console.log("!==",op1 !== op2);
+console.log("<=",op1 <= op2);
+console.log(">=",op1 >= op2);
+
+console.log("Logical operators");
+console.log("&& => ",op1 > op2 && op1 < op2);
+console.log("|| => ",op1 > op2 || op1 < op2);
+console.log("!",! op1 < op2);
