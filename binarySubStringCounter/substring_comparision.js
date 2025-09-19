898c35361bbe339d559999dc07597f3f1322aabd
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-19 17:51:58 +0530

    modify to count the length of substring

--- binarySubStringCounter/substring_comparision.js
+++ binarySubStringCounter/substring_comparision.js
@@ -1,3 +1,13 @@
 const substring = 10;
 let binaryNumber = 1000001;
+let lengthOfSubstring = 0;
+let remainder = 0;
+let duplicatedSubstring = substring;
+
+remainder = duplicatedSubstring % 2;
+remainder && duplicatedSubstring--;
+lengthOfSubstring++;
+duplicatedSubstring = duplicatedSubstring / 10;
+
+console.log(duplicatedSubstring)
 console.log(substring, binaryNumber, "occurance is 1");
