ba85166e2c51bbe6e9b96f2428cad43c4636b218
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-19 13:52:07 +0530

    Created switch_example.js file with code

---
+++ decisionMaking/switch_example.js
@@ -0,0 +1,17 @@
+const fruit = "Apple";
+
+switch (fruit) {
+    case "Banana": 
+        console.log("it tastes sweet");
+        break;
+    case "Apple": 
+        console.log("it tastes tart");
+        break;
+    case "Amla": 
+        console.log("it tastes Sour");
+        break;
+    case "Grape": 
+        console.log("it tastes Tangy");
+        break;
+    default : console.log("I dont have that fruit");
+}
