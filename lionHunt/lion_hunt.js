3b6125547c3045c5a9744f5c29298bed705c9553
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 15:37:55 +0530

    Update to find number of spaces

--- lionHunt/lion_hunt.js
+++ lionHunt/lion_hunt.js
@@ -1,2 +1,11 @@
-const input = "LZ";
-console.log("LZ => 0");
+const input = "L  Z";
+const space = " ";
+let distance = 0;
+let output = -1;
+let animal = input[0];
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === " ") {
+    distance++;
+  }
+}
+console.log(distance);
