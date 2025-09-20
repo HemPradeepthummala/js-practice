b61a50e0593b413eda4c6feed4fd146bd2718289
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 16:48:15 +0530

    Update remove_leading_spaces.js to find the characeter

--- lionHunt/remove_leading_spaces.js
+++ lionHunt/remove_leading_spaces.js
@@ -1,7 +1,14 @@
-const input = "   hem";
+const input = "  hem";
 const space = " ";
 let duplicatedString = "";
+
 input[0] !== space && (duplicatedString = input[0]);
+console.log(duplicatedString);
+
+input[1] !== space && (duplicatedString = input[1]);
+console.log(duplicatedString);
+
+input[2] !== space && (duplicatedString = input[2]);
 console.log(duplicatedString);
 
 console.log(input, "=> 'hem'");
