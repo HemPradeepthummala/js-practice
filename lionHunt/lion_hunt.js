7ee3e95596aadee4cc5feecf9a32227f68947709
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 16:28:20 +0530

    add compareOutput variable to compare output and distance

--- lionHunt/lion_hunt.js
+++ lionHunt/lion_hunt.js
@@ -3,15 +3,18 @@ const space = " ";
 let distance = 0;
 let output = -1;
 let animal = input[0];
+
 for (let index = 0; index < input.length; index++) {
   input[index] === space && distance++;
   input[index] === animal && (distance = 0);
   if (input[index] !== animal && input[index] !== space) {
-    if (output === -1 || output > distance) {
+    let compareOutput = output === -1 || output > distance;
+    if (compareOutput) {
       output = distance;
       distance = 0;
       animal = input[index];
     }
   }
 }
+
 console.log(distance, "=> distance", output, "=> output");
