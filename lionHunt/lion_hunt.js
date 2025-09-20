aae1ac6773d5092ad8f2fa25d83f7d58a7020b10
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 16:15:09 +0530

    Update lion_hunt.js to display output

--- lionHunt/lion_hunt.js
+++ lionHunt/lion_hunt.js
@@ -1,11 +1,21 @@
-const input = "L  Z";
+const input =  "L  ZL Z";
 const space = " ";
 let distance = 0;
 let output = -1;
 let animal = input[0];
-for (let i = 0; i < input.length; i++) {
-  if (input[i] === " ") {
+for (let index = 0; index < input.length; index++) {
+  if (input[index] === " ") {
     distance++;
   }
+  if (input[index] === animal) {
+    distance = 0;
+  }
+  if (input[index] !== animal && input[index] !== space) {
+    if (output === -1 || output > distance) {
+      output = distance;
+      distance = 0;
+      animal = input[index];
+    }
+  }
 }
-console.log(distance);
+console.log(distance, "=> distance", output, "=> output");
