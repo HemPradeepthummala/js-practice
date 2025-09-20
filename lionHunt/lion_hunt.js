1e6d069a1dcac0a6751a8116a6ade92430ad463a
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 16:19:27 +0530

    replace if with logical && in loop

--- lionHunt/lion_hunt.js
+++ lionHunt/lion_hunt.js
@@ -1,15 +1,11 @@
-const input =  "L  ZL Z";
+const input = "L  ZL Z";
 const space = " ";
 let distance = 0;
 let output = -1;
 let animal = input[0];
 for (let index = 0; index < input.length; index++) {
-  if (input[index] === " ") {
-    distance++;
-  }
-  if (input[index] === animal) {
-    distance = 0;
-  }
+  input[index] === space && distance++;
+  input[index] === animal && (distance = 0);
   if (input[index] !== animal && input[index] !== space) {
     if (output === -1 || output > distance) {
       output = distance;
