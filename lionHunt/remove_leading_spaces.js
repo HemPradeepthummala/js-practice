e39280cccea65ec75ad55c3519b36e752f7622d2
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-20 16:46:18 +0530

    Update remove_leading_spaces.js to check for spaces

--- lionHunt/remove_leading_spaces.js
+++ lionHunt/remove_leading_spaces.js
@@ -1,1 +1,7 @@
+const input = "   hem";
+const space = " ";
+let duplicatedString = "";
+input[0] !== space && (duplicatedString = input[0]);
+console.log(duplicatedString);
 
+console.log(input, "=> 'hem'");
