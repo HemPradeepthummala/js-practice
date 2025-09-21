ffabba347294084d226707d5fee4bca54536838a
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-21 14:37:25 +0530

    compare strings and print occurrence

--- binarySubStringCounter/usingStrings/binary_substring_counter.js
+++ binarySubStringCounter/usingStrings/binary_substring_counter.js
@@ -12,8 +12,30 @@ while (number !== 0) {
 
 let occuranceCounter = 0;
 let comparableString = "";
+
 comparableString = resultedBinary[0] + resultedBinary[1];
 subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
+
+comparableString = resultedBinary[1] + resultedBinary[2];
+subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
+
+comparableString = resultedBinary[2] + resultedBinary[3];
+subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
+
+comparableString = resultedBinary[3] + resultedBinary[4];
+subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
+
+comparableString = resultedBinary[4] + resultedBinary[5];
+subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
+
+comparableString = resultedBinary[5] + resultedBinary[6];
+subString === comparableString && occuranceCounter++;
+console.log("comparableString",comparableString);
 
 console.log("occuranceCounter =>", occuranceCounter);
 console.log("number =>", number, "resultedBinary =>", resultedBinary);
