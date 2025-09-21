4be71d17e8687049de15901bf264096c2f0e27a2
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-21 13:32:49 +0530

    add loop to find binaryNumber

--- binarySubStringCounter/usingStrings/binary_substring_counter.js
+++ binarySubStringCounter/usingStrings/binary_substring_counter.js
@@ -3,52 +3,12 @@ const subString = "10";
 let number = inputNumber;
 let resultedBinary = "";
 
-let remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
-
-console.log("number =>", number, "resultedBinary =>", resultedBinary);
-
-remainder = number % 2;
-resultedBinary = remainder + resultedBinary;
-remainder && (number -= 1);
-number = number / 2;
+while (number !== 0) {
+  let remainder = number % 2;
+  resultedBinary = remainder + resultedBinary;
+  remainder && (number -= 1);
+  number = number / 2;
+}
 
 console.log("number =>", number, "resultedBinary =>", resultedBinary);
 console.log(inputNumber, subString, "count => 1");
