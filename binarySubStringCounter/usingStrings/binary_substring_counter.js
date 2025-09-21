e8482da0989385aeeafb424fd2dc576e6e4e527d
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-21 13:15:30 +0530

    find the first bit of binary number

--- binarySubStringCounter/usingStrings/binary_substring_counter.js
+++ binarySubStringCounter/usingStrings/binary_substring_counter.js
@@ -1,3 +1,12 @@
 const inputNumber = 65;
 const subString = "10";
+let number = inputNumber;
+let resultedBinary = "";
+
+let remainder = number%2;
+resultedBinary = resultedBinary + remainder;
+remainder && (number -= 1);
+number = number / 2;
+
+console.log("number =>",number,"resultedBinary =>", resultedBinary);
 console.log(inputNumber, subString, "count => 1");
