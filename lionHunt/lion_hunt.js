9b3291add56a3f70070c1ecc6c4973b51a63c63f
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-22 09:45:44 +0530

    modify shortcuts to if statements, and rename duplicatedString

--- lionHunt/lion_hunt.js
+++ lionHunt/lion_hunt.js
@@ -2,25 +2,29 @@ const inputString = "L Z   L L  LZ";
 const space = " ";
 let distance = 0;
 let output = -1;
-let duplicatedString = "";
+let stringWithNoLeadingSpaces = "";
 
 for (let index = 0; index < inputString.length; index++) {
-  if (inputString[index] !== space || duplicatedString !== "") {
-    duplicatedString = duplicatedString + inputString[index];
+  if (inputString[index] !== space || stringWithNoLeadingSpaces !== "") {
+    stringWithNoLeadingSpaces = stringWithNoLeadingSpaces + inputString[index];
   }
 }
 
-let animal = duplicatedString[0];
+let animal = stringWithNoLeadingSpaces[0];
 
-for (let index = 0; index < duplicatedString.length; index++) {
-  duplicatedString[index] === space && distance++;
-  duplicatedString[index] === animal && (distance = 0);
-  if (duplicatedString[index] !== animal && duplicatedString[index] !== space) {
-    let compareOutput = output === -1 || output > distance;
+for (let index = 0; index < stringWithNoLeadingSpaces.length; index++) {
+  if (stringWithNoLeadingSpaces[index] === space){
+    distance++;
+  } 
+  if (stringWithNoLeadingSpaces[index] === animal){
+    (distance = 0);
+  }
+  if (stringWithNoLeadingSpaces[index] !== animal && stringWithNoLeadingSpaces[index] !== space) {
+    let compareOutput = (output === -1 || output > distance);
     if (compareOutput) {
       output = distance;
     }
-    animal = duplicatedString[index];
+    animal = stringWithNoLeadingSpaces[index];
     distance = 0;
   }
 }
