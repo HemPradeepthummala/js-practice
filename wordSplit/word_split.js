fe5ad8233303f2bdd2790bd4379ab024be84f08c
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-09-27 15:27:28 +0530

    create test functions

--- wordSplit/word_split.js
+++ wordSplit/word_split.js
@@ -1,8 +1,21 @@
-function splitWord(input){
+function splitWord(input) {
 
 }
 
-function testAll(){
+function composeMessage(input, expected, actual) {
+  const symbol = actual === expected ? '✅' : '❌';
+  const inputMessage = ' | ' + input + ' | ';
+  const outcomeMessage = expected + ' | ' + actual + ' |';
+  return symbol + inputMessage + outcomeMessage;
+}
+
+function testSplitWord(input, expected) {
+  const actual = splitWord(input);
+  const message = composeMessage(input, expected, actual);
+  console.log(message);
+}
+
+function testAll() {
   testSplitWord('apple', 'ape,p,l');
 }
 
