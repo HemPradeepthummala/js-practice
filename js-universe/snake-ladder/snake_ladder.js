44af7d704fc0017a7ecb25983e81d91faf56dff9
Hem Pradeep Royal Thummala <172028260+HemPradeepthummala@users.noreply.github.com>
2025-10-13 15:20:01 +0530

    Update snake_ladder.js

--- js-universe/snake-ladder/snake_ladder.js
+++ js-universe/snake-ladder/snake_ladder.js
@@ -2,22 +2,23 @@ const ROWS = 10;
 const COLUMNS = 10;
 
 function pad(value, length = 2) {
-  return value.toString().padStart(length, '0')
+  return value.toString().padStart(length, '_')
 }
 
 function makeRow(size, row) {
   const array = [];
 
   for (let index = 0; index < size; index++) {
-    array.push(pad(index + 1 + row * 10));
+    array.push('__');
   }
   return array;
 }
 
 function displayBoard(array) {
   const board = [];
+  board.unshift(' __'.repeat(array[0].length))
   for (let row = 0; row < ROWS; row++) {
-    board.push(array[row].join('|'));
+    board.push('|' + array[row].join('|')+'|');
   }
   console.log(board.join('\n'))
 }
